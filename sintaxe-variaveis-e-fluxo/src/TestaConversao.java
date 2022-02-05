3c9d5136073999c6af6cfab7553fc2e077dd5d6e
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-05 15:23:29 -0300

    END chapter 4

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,27 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		
+		//double salario = 1250.50;
+		double salario = 1270.50;
+		
+		// Casting => Forca o valor de uma variável double 
+		// em um int (pode ser usada com outros tipos)
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 21025185015125151l;
+		short valorPequeno = 1214;
+		byte b = 120;
+		
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		
+		// Double para trabalhar com dinheiro nao presta muito!
+		System.out.println(valor1 + valor2);
+		
+		
+	}
+
+}
