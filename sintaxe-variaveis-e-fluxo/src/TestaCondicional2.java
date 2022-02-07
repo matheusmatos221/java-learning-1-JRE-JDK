64346bef975ef3754c3e9028955b63bd8d3ca78c
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-07 10:55:16 -0300

    Faz uso de booleano com condicional

--- sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -4,10 +4,11 @@ public class TestaCondicional2 {
 	public static void main(String[] args) {		
 		System.out.println("Testando condicionais:");
 		
-		int idade = 18;
-		int quantidadePessoas = 1;
+		int idade = 16;
+		int quantidadePessoas = 2;
+		boolean acompanhado = quantidadePessoas >= 2;
 		
-		if(idade>= 18 || quantidadePessoas>=2) {
+		if(idade>= 18 || acompanhado) {
 			System.out.println("Voce pode entrar!");
 		}else {
 			System.out.println("Voce nao pode entrar!");				
