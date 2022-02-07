59c49e0acbad6853a0301bb93e7dc9916c7b64d0
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-07 15:13:00 -0300

    Exibe apenas o resultado final da soma

--- sintaxe-variaveis-e-fluxo/src/TesteSomatoria.java
+++ sintaxe-variaveis-e-fluxo/src/TesteSomatoria.java
@@ -2,13 +2,13 @@
 public class TesteSomatoria {
 	public static void main (String[] args) {
 		int contador = 0;
+		int total = 0;
+		
 		while(contador<=10) {
-			int total = 0;
 			total = total + contador;
-			
-			System.out.println(total);
-			contador++;
+			contador++;			
 		}
+		System.out.println(total);
 		
 	}
 }
