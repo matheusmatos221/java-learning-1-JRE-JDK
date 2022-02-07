5e1bb58067c98769bbc5a4141c23136979460635
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-07 16:49:03 -0300

    Constroi a tabuada do 1 ao 10 usando laços encadeados, for dentro de for

---
+++ sintaxe-variaveis-e-fluxo/src/TransformarWhileEmFor.java
@@ -0,0 +1,21 @@
+
+public class TransformarWhileEmFor {
+	
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
+
+/* RESPOSTA:
+public class ForInsteadWhile {
+	public static void (String[] args) {
+		for(int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
+*/
